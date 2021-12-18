ff5b65e7463f924266ea87aa52cef64e9e6ca302
evodable <vodableemma@gmail.com>
2021-12-18 21:48:27 +0100

    [Level] Load first sub level with c++ script

---
+++ Source/Proto_Boat/SProceduralLevel.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SProceduralLevel.h"
+#include "Runtime/Engine/Classes/Engine/LevelStreaming.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+
+// Sets default values
+ASProceduralLevel::ASProceduralLevel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    ProceduralWorld = GetWorld();
+
+    Rooms.Add(RoomType::START, {});
+    Rooms.Add(RoomType::END, {});
+    Rooms.Add(RoomType::BACKTOFRONT, {});
+    Rooms.Add(RoomType::RIGHTTOLEFT, {});
+    Rooms.Add(RoomType::LEFTTOBACK, {});
+    Rooms.Add(RoomType::FRONTTORIGHT, {});
+
+
+}
+
+// Called when the game starts or when spawned
+void ASProceduralLevel::BeginPlay()
+{
+
+    const TArray<ULevelStreaming*>& streamedLevels = ProceduralWorld->GetStreamingLevels();
+    for (ULevelStreaming* streamingLevel : streamedLevels)
+    {
+        // Sort all sub Levels
+        FString levelName = streamingLevel->GetWorldAssetPackageName();
+        if (levelName.Contains(TEXT("Start"))) 
+            Rooms.Find(RoomType::START)->RoomsName.Add(streamingLevel->GetWorldAssetPackageName());
+        else if (levelName.Contains(TEXT("End"))) 
+            Rooms.Find(RoomType::END)->RoomsName.Add(streamingLevel->GetWorldAssetPackageName());
+        else if (levelName.Contains(TEXT("Lt"))) 
+            Rooms.Find(RoomType::BACKTOFRONT)->RoomsName.Add(streamingLevel->GetWorldAssetPackageName());
+        else if (levelName.Contains(TEXT("Bt"))) 
+            Rooms.Find(RoomType::RIGHTTOLEFT)->RoomsName.Add(streamingLevel->GetWorldAssetPackageName());
+        else if (levelName.Contains(TEXT("Lb"))) 
+            Rooms.Find(RoomType::LEFTTOBACK)->RoomsName.Add(streamingLevel->GetWorldAssetPackageName());
+
+
+        UE_LOG(LogTemp, Warning, TEXT("LEVEL NAME : %s"), *levelName);
+    }
+
+    if (Rooms.Find(RoomType::START)) {
+        FString levelName = Rooms.Find(RoomType::START)->RoomsName[0];
+        UE_LOG(LogTemp, Warning, TEXT("START LVL : %s"), *levelName);
+        UGameplayStatics::LoadStreamLevel(this, *levelName, true, true, FLatentActionInfo());
+    }
+
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ASProceduralLevel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Proto_Boat/SProceduralLevel.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SRoom.h"
+#include "SProceduralLevel.generated.h"
+
+
+UENUM(BlueprintType)
+enum class RoomType : uint8
+{
+	START = 0			UMETA(DisplayName = "Start"),
+	END = 1				UMETA(DisplayName = "End"),
+	LEFTTOBACK = 2		UMETA(DisplayName = "Left to Back"),
+	FRONTTORIGHT = 3	UMETA(DisplayName = "Front to Right"),
+	RIGHTTOLEFT = 4		UMETA(DisplayName = "Right to Left"),
+	BACKTOFRONT	= 5		UMETA(DisplayName = "Back to Front"),
+};
+
+USTRUCT()
+struct FRooms
+{
+	GENERATED_BODY()
+
+	TArray< FString > RoomsName;
+};
+
+
+
+UCLASS()
+class PROTO_BOAT_API ASProceduralLevel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASProceduralLevel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+		TMap< RoomType, FRooms > Rooms;
+	
+	UPROPERTY(EditAnywhere)
+		FVector RoomSize;
+
+	UPROPERTY(EditAnyWhere)
+		float OffsetBetweenRoom;
+
+	UPROPERTY(EditAnywhere)
+		UWorld* ProceduralWorld;
+};
